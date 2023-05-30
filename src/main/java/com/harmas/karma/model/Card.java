f7d140c729f5e6ec295a82657f2e49f31d9e82a8
MattheWhite <fehermatyas97@gmail.com>
2023-05-30 15:34:29 +0200

    Extend Card class declaration with more fields, constructor, getter.

--- src/main/java/com/harmas/karma/model/Card.java
+++ src/main/java/com/harmas/karma/model/Card.java
@@ -1,4 +1,15 @@
 package com.harmas.karma.model;
 
 public class Card {
+    private final CardType type;
+    private final ColorType color;
+
+    public Card(CardType type, ColorType color) {
+        this.type = type;
+        this.color = color;
+    }
+
+    public Integer getValue() {
+        return this.type.getValue();
+    }
 }
