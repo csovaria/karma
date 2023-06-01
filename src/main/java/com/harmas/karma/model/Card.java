29b3106738b7e14e4f24e9344d1e3451336130ce
danieldudas91 <duddaniel0919@gmail.com>
2023-06-01 13:45:27 +0200

    override toString method and move id generating to constructor

--- src/main/java/com/harmas/karma/model/Card.java
+++ src/main/java/com/harmas/karma/model/Card.java
@@ -3,15 +3,15 @@ package com.harmas.karma.model;
 import java.util.UUID;
 
 public class Card {
-    private final UUID id = UUID.randomUUID();
+    private final UUID id;
     private final CardValue value;
     private final CardColor color;
 
     public Card(CardValue value, CardColor color) {
         this.value = value;
         this.color = color;
+        this.id = UUID.randomUUID();
     }
-
     public Integer getValue() {
         return this.value.getValue();
     }
@@ -22,5 +22,14 @@ public class Card {
 
     public UUID getId() {
         return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "id=" + id +
+                ", value=" + value +
+                ", color=" + color +
+                '}';
     }
 }
