be79f27846197ee4acc74fad5b753caf1a5d4a28
Andras Csovari <csovaria@gmail.com>
2023-06-12 21:36:15 +0200

    migrate functionality to the service layer

--- src/main/java/com/harmas/karma/model/Game.java
+++ src/main/java/com/harmas/karma/model/Game.java
@@ -8,96 +8,53 @@ import java.util.stream.Collectors;
 
 @Component
 public class Game {
-    private List<Player> players;
-    private Player currentPlayer;
-    private LinkedList<Card> drawDeck;
-    private LinkedList<Card> burnDeck;
-    private LinkedList<Card> playDeck;
-    private static final int NUMBER_OF_DECKS = 1;
-    private static final int NUMBER_OF_PLAYERS = 2;
 
-    public Game() {
-        burnDeck = new LinkedList<>();
-        playDeck = new LinkedList<>();
-        drawDeck = new LinkedList<>();
-        players = new ArrayList<>();
-        generateDecks();
-        generatePlayers();
-        dealCards();
-    }
-
-
-    private void generatePlayers() {
-        for (int i = 0; i < NUMBER_OF_PLAYERS; i++) {
-            players.add(new Player("Player " + i));
-        }
-    }
-
-
-    public void generateDecks() {
-        for (int i = 0; i < NUMBER_OF_DECKS; i++) {
-            for (CardValue card : CardValue.values()) {
-                for (CardColor type : CardColor.values()) {
-                    drawDeck.add(new Card(card, type));
-                }
-            }
-        }
-        Collections.shuffle(drawDeck);
-    }
-
-
-    private void dealCards() {
-        playDeck.add(drawDeck.pop());
-        for (Player player : players) {
-            List<Card> cards = new ArrayList<>(drawDeck.subList(0, 2));
-            drawDeck.subList(0, 2).clear();
-            player.pullCards(cards);
-        }
-    }
-
-
-    public boolean isCardCanBePlaced(Card card) {
-        Card topCard = playDeck.getFirst();
-        return card.getValue() >= topCard.getValue()
-                || card.getValue().equals(CardValue.TWO.getValue())
-                || card.getValue().equals(CardValue.TEN.getValue());
-    }
-
-
-    public boolean areCardsValuesEquals(List<Card> cards) {
-        return cards.stream().allMatch(card -> card.getValue().equals(cards.get(0).getValue()));
-    }
-
-
-    public boolean areCardsCanBePlaced(List<Card> cards) {
-        if (!areCardsValuesEquals(cards)) return false;
-        return isCardCanBePlaced(cards.get(0));
-    }
-
-
-    public void placeCardsOnPlayDeck(List<Card> cards) {
-        playDeck.addAll(cards);
-    }
-
-
-    public boolean playCardFromHand(List<Card> cards) {
-        if (areCardsCanBePlaced(cards)) {
-            placeCardsOnPlayDeck(cards);
-            return true;
-        }
-        return false;
-    }
-
-
-    public boolean playerHasCards(List<Card> cards) {
-        List<UUID> cardIDs = cards.stream().map(Card::getId).toList();
-        return new HashSet<>(cardIDs).containsAll(currentPlayer.getCardIDs());
-    }
-
-
-    public void drawPlayDeck() {
-        currentPlayer.pullCards(playDeck);
-        playDeck.remove();
-        playDeck.add(drawDeck.pop());
-    }
+//    public boolean isCardCanBePlaced(Card card) {
+//        Card topCard = playDeck.getFirst();
+//        return card.getValue() >= topCard.getValue()
+//                || card.getValue().equals(CardValue.TWO.getValue())
+//                || card.getValue().equals(CardValue.TEN.getValue());
+//    }
+//
+//
+//    public boolean areCardsValuesEquals(List<Card> cards) {
+//        return cards.stream().allMatch(card -> card.getValue().equals(cards.get(0).getValue()));
+//    }
+//
+//
+//    public boolean areCardsCanBePlaced(List<Card> cards) {
+//        if (!areCardsValuesEquals(cards)) return false;
+//        return isCardCanBePlaced(cards.get(0));
+//    }
+//
+//
+//    public void placeCardsOnPlayDeck(List<Card> cards) {
+//        playDeck.addAll(cards);
+//    }
+//
+//
+//    public boolean playCardFromHand(List<Card> cards) {
+//        if (areCardsCanBePlaced(cards)) {
+//            placeCardsOnPlayDeck(cards);
+//            return true;
+//        }
+//        return false;
+//    }
+//
+//
+//    public boolean playerHasCards(List<Card> cards) {
+//        List<UUID> cardIDs = cards.stream().map(Card::getId).toList();
+//        return new HashSet<>(cardIDs).containsAll(currentPlayer.getCardIDs());
+//    }
+//
+//
+//    public void drawPlayDeck() {
+//        currentPlayer.pullCards(playDeck);
+//        playDeck.remove();
+//        playDeck.add(drawDeck.pop());
+//    }
+//
+//    public Player getCurrentPlayer() {
+//        return currentPlayer;
+//    }
 }
--- src/main/java/com/harmas/karma/service/GameService.java
+++ src/main/java/com/harmas/karma/service/GameService.java
@@ -1,17 +1,43 @@
 package com.harmas.karma.service;
 
 import com.harmas.karma.model.Card;
-import com.harmas.karma.model.Game;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.harmas.karma.model.CardColor;
+import com.harmas.karma.model.CardValue;
+import com.harmas.karma.repository.DAO.DeckDAO;
+import com.harmas.karma.repository.DAO.PlayerDAO;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.Collections;
+import java.util.LinkedList;
 
 @Service
 public class GameService {
-    @Autowired
-    private Game game;
-    public boolean playCardFromHand(List<Card> cards) {
-        return game.playCardFromHand(cards);
+    private DeckDAO deckMemory;
+    private PlayerDAO playerMemory;
+
+    public GameService(DeckDAO deckMemory, PlayerDAO playerMemory) {
+        this.deckMemory = deckMemory;
+        this.playerMemory = playerMemory;
+    }
+
+//    public boolean playCardFromHand(List<Card> cards) {
+//        return game.playCardFromHand(cards);
+//    }
+//
+//    public List<Card> getCurrentPlayerCards(){
+//        return game.getCurrentPlayer().getCards()
+//    }
+
+
+
+    public LinkedList<Card> generateDrawDeck() {
+        LinkedList<Card> cards = new LinkedList<>();
+        for (CardValue card : CardValue.values()) {
+            for (CardColor type : CardColor.values()) {
+                cards.add(new Card(card, type));
+            }
+        }
+        Collections.shuffle(cards);
+        return cards;
     }
 }
