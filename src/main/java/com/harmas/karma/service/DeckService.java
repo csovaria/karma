b2252df2e3148844ea08939d0e42a5c2629c58e1
danieldudas91 <duddaniel0919@gmail.com>
2023-06-13 13:17:33 +0200

    create DeckDAO autowired field

--- src/main/java/com/harmas/karma/service/DeckService.java
+++ src/main/java/com/harmas/karma/service/DeckService.java
@@ -1,5 +1,14 @@
 package com.harmas.karma.service;
 
+import com.harmas.karma.repository.DAO.DeckDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class DeckService {
+
+    @Autowired
+    private DeckDAO deckDAO;
 
 }
