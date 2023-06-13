6433050fe6780f2331af93d8de75449734b63cf5
danieldudas91 <duddaniel0919@gmail.com>
2023-06-13 13:13:00 +0200

    implement instantiatePlayers method

--- src/main/java/com/harmas/karma/service/PlayerService.java
+++ src/main/java/com/harmas/karma/service/PlayerService.java
@@ -1,5 +1,14 @@
 package com.harmas.karma.service;
 
+import com.harmas.karma.model.Player;
+import com.harmas.karma.repository.DAO.PlayerDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class PlayerService {
 
     @Autowired
@@ -8,4 +17,11 @@ public class PlayerService {
     @Autowired
     private DeckService deckService;
 
+    public void instantiatePlayers(int numberOfPlayers) {
+        List<Player> players = new ArrayList<>(numberOfPlayers);
+        for (int i = 0; i < numberOfPlayers; i++){
+            players.add(new Player("Player " + i));
+        }
+        playerDAO.savePlayers(players);
+    }
 }
