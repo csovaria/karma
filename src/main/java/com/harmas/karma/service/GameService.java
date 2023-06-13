2bd2a605e7fc2749bb597113735bdff80481f67f
danieldudas91 <duddaniel0919@gmail.com>
2023-06-13 13:19:16 +0200

    implement generateDrawDeck and initializeDrawDeck methods

--- src/main/java/com/harmas/karma/service/GameService.java
+++ src/main/java/com/harmas/karma/service/GameService.java
@@ -1,26 +1,21 @@
 package com.harmas.karma.service;
 
-import com.harmas.karma.model.Card;
-import com.harmas.karma.model.CardColor;
-import com.harmas.karma.model.CardValue;
-import com.harmas.karma.repository.DAO.DeckDAO;
-import com.harmas.karma.repository.DAO.PlayerDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Collections;
-import java.util.LinkedList;
 
 @Service
 public class GameService {
-    private DeckDAO deckMemory;
-    private PlayerDAO playerMemory;
 
-    public GameService(DeckDAO deckMemory, PlayerDAO playerMemory) {
-        this.deckMemory = deckMemory;
-        this.playerMemory = playerMemory;
+    private DeckService deckService;
+    private PlayerService playerService;
+    private static final int NUMBER_OF_PLAYERS = 2;
+    @Autowired
+    public GameService(DeckService deckService, PlayerService playerService) {
+        this.deckService = deckService;
+        this.playerService = playerService;
     }
 
-//    public boolean playCardFromHand(List<Card> cards) {
+    //    public boolean playCardFromHand(List<Card> cards) {
 //        return game.playCardFromHand(cards);
 //    }
 //
@@ -28,23 +23,13 @@ public class GameService {
 //        return game.getCurrentPlayer().getCards()
 //    }
     public void initializeGame() {
-        deckMemory.initializeDrawDeck(generateDrawDeck());
-        deckMemory.placeCards(deckMemory.drawCards(1));
-        playerMemory.instantiatePlayers();
-        playerMemory.pullCards(deckMemory.drawCards(3));
-        playerMemory.moveToTheNextPlayer();
-        playerMemory.pullCards(deckMemory.drawCards(3));
+        deckService.initializeDrawDeck();
+        deckService.playCardFromDrawDeck();
+        playerService.instantiatePlayers(NUMBER_OF_PLAYERS);
+        handCardsToPlayers(3);
     }
 
-
-    public LinkedList<Card> generateDrawDeck() {
-        LinkedList<Card> cards = new LinkedList<>();
-        for (CardValue card : CardValue.values()) {
-            for (CardColor type : CardColor.values()) {
-                cards.add(new Card(card, type));
-            }
-        }
-        Collections.shuffle(cards);
-        return cards;
+    public void handCardsToPlayers(int numberOfCards){
+        playerService.handCardsToPlayers(numberOfCards);
     }
 }
