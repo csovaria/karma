f44f8b2edbabb46fee6446c53ea3683c5e69b77e
danieldudas91 <duddaniel0919@gmail.com>
2023-06-13 13:21:08 +0200

    add savePlayers method

--- src/main/java/com/harmas/karma/repository/DAO/PlayerDAO.java
+++ src/main/java/com/harmas/karma/repository/DAO/PlayerDAO.java
@@ -1,11 +1,12 @@
 package com.harmas.karma.repository.DAO;
 
 import com.harmas.karma.model.Card;
+import com.harmas.karma.model.Player;
 
 import java.util.List;
 
 public interface PlayerDAO {
-    void instantiatePlayers();
+    void savePlayers(List<Player> players);
 
     void pullCards(List<Card> cards);
 
