0c2787f3e58505216e22157afa8b015c91084952
MattheWhite <fehermatyas97@gmail.com>
2023-06-13 21:06:28 +0200

    Declare getPlayerByID() method signature in PlayerDAO interface.

--- src/main/java/com/harmas/karma/repository/DAO/PlayerDAO.java
+++ src/main/java/com/harmas/karma/repository/DAO/PlayerDAO.java
@@ -4,6 +4,7 @@ import com.harmas.karma.model.Card;
 import com.harmas.karma.model.Player;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface PlayerDAO {
     void savePlayers(List<Player> players);
@@ -13,4 +14,6 @@ public interface PlayerDAO {
     void moveToTheNextPlayer();
 
     void handCardsToPlayers(List<Card> cards);
+
+    Player getPlayerByID(UUID id);
 }
