9aa19e415e0629f23c961a613e64115225ef86ff
Andras Csovari <csovaria@gmail.com>
2023-06-14 14:31:00 +0200

    change model structure

--- src/main/java/com/harmas/karma/model/Deck.java
+++ src/main/java/com/harmas/karma/model/Deck.java
@@ -1,6 +1,19 @@
 package com.harmas.karma.model;
 
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Deck {
 
+    protected LinkedList<Card> cards;
+
+    protected Deck() {
+        this.cards = new LinkedList<>();
+    }
+
+    protected void saveCards(List<Card> cards){
+        this.cards.addAll(cards);
+    }
 }
